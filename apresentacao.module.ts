97b939638ee9ebacb78070167dba6195dfd14891
Pedro Ivo <116113018+pedroiv2006@users.noreply.github.com>
2024-09-10 15:47:59 -0300

    Add files via upload

--- apresentacao.module.ts
+++ apresentacao.module.ts
@@ -1,21 +1,21 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { IonicModule } from '@ionic/angular';
 
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { IonicModule } from "@ionic/angular";
-import { ApresentacaoPageRoutingModule } from "src/app/apresentacao/apresentacao-routing.module";
-import { CommonModule } from "@angular/common";
-import { Etapa1Page } from "src/app/apresentacao/etapa1/etapa1.page";
-import { ApresentacaoPage } from "src/app/apresentacao/apresentacao.page";
+import { ApresentacaoPageRoutingModule } from './apresentacao-routing.module';
 
-    @NgModule({
-        imports: [
-            CommonModule,
-            FormsModule,
-            IonicModule,
-            ApresentacaoPageRoutingModule,
-        ],
-         declarations: [ApresentacaoPage,Etapa1Page],
-         schemas:[CUSTOM_ELEMENTS_SCHEMA]
-    })
-    export class ApresentacaoPageModule{}
+import { ApresentacaoPage } from './apresentacao.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ApresentacaoPageRoutingModule
+  ],
+  declarations: [ApresentacaoPage],
+  schemas:[ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class ApresentacaoPageModule {}
